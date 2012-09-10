90119839b7a8235f9e92e12b729766089a779fc1
Shorn <hg-shorn@sneakemail.com>
2012-09-10 20:52:35 +1000

    add a comment

--- main/prod-src/groovy/net/intellij/plugins/sbt/cla/config/IdeaDialogWrapper.groovy
+++ main/prod-src/groovy/net/intellij/plugins/sbt/cla/config/IdeaDialogWrapper.groovy
@@ -3,6 +3,10 @@ package net.intellij.plugins.sbt.cla.config
 import com.intellij.openapi.ui.DialogWrapper
 import javax.swing.JComponent
 
+/**
+ * replaced by a rewritten java version, because I think this was
+ * causing weird packaging issues (the noclassdef found problem).
+ */
 class IdeaDialogWrapper extends DialogWrapper {
   JComponent preferredFocus
   JComponent centerPanel
